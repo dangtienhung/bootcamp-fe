2052a77cff35961f9c2839fc6e4e3e34c01b523d
cô giáo hưng <hungdang02042003@gmail.com>
2024-01-18 21:58:56 +0700

    buổi 5: children

--- fe-react-ts/src/components/buoi5/count.tsx
+++ fe-react-ts/src/components/buoi5/count.tsx
@@ -8,28 +8,27 @@ const CountBuoi5 = () => {
 	};
 	return (
 		<div className="w-[300px] mx-auto">
-			<ButtonCount handleClicked={handleClick} countNumber={count} />
+			<ButtonCount>
+				<div>
+					<div>{count}</div>
+					<button
+						className="bg-green-500 px-2 py-1 rounded text-white"
+						onClick={() => handleClick()}
+					>
+						Tăng số
+					</button>
+				</div>
+			</ButtonCount>
 		</div>
 	);
 };
 
 interface IButtonCountProps {
-	handleClicked: () => void;
-	countNumber: number;
+	children: React.ReactNode;
 }
 
-const ButtonCount = ({ handleClicked, countNumber }: IButtonCountProps) => {
-	return (
-		<div>
-			<div>{countNumber}</div>
-			<button
-				className="bg-green-500 px-2 py-1 rounded text-white"
-				onClick={() => handleClicked()}
-			>
-				Tăng số
-			</button>
-		</div>
-	);
+const ButtonCount = ({ children }: IButtonCountProps) => {
+	return <div>{children}</div>;
 };
 
 export default CountBuoi5;
