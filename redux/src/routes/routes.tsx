ffa04cbeb31f3e54c7bd2d18f1c98b6a73e9a3a0
cô giáo hưng <hungdang02042003@gmail.com>
2024-03-14 22:01:26 +0700

    buổi 22: get one/ update product

--- redux/src/routes/routes.tsx
+++ redux/src/routes/routes.tsx
@@ -1,3 +1,4 @@
+import DetailPage from '../pages/detail/detail-page';
 import HomePage from '../pages/home/home-page';
 import { createBrowserRouter } from 'react-router-dom';
 
@@ -5,5 +6,9 @@ export const router = createBrowserRouter([
 	{
 		path: '/home',
 		element: <HomePage />,
+	},
+	{
+		path: '/detail/:id',
+		element: <DetailPage />,
 	},
 ]);
