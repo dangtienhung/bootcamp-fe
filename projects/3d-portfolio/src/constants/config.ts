6969e246b8c7edfa375be14ca2c7dbdab1033d34
cô giáo hưng <hungdang02042003@gmail.com>
2024-03-21 22:18:07 +0700

    update code

--- projects/3d-portfolio/src/constants/config.ts
+++ projects/3d-portfolio/src/constants/config.ts
@@ -1,94 +1,94 @@
 type TSection = {
-  p: string;
-  h2: string;
-  content?: string;
-};
+  p: string
+  h2: string
+  content?: string
+}
 
 type TConfig = {
   html: {
-    title: string;
-    fullName: string;
-    email: string;
-  };
+    title: string
+    fullName: string
+    email: string
+  }
   hero: {
-    name: string;
-    p: string[];
-  };
+    name: string
+    p: string[]
+  }
   contact: {
     form: {
       name: {
-        span: string;
-        placeholder: string;
-      };
+        span: string
+        placeholder: string
+      }
       email: {
-        span: string;
-        placeholder: string;
-      };
+        span: string
+        placeholder: string
+      }
       message: {
-        span: string;
-        placeholder: string;
-      };
-    };
-  } & TSection;
+        span: string
+        placeholder: string
+      }
+    }
+  } & TSection
   sections: {
-    about: Required<TSection>;
-    experience: TSection;
-    feedbacks: TSection;
-    works: Required<TSection>;
-  };
-};
+    about: Required<TSection>
+    experience: TSection
+    feedbacks: TSection
+    works: Required<TSection>
+  }
+}
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: '3D Test',
+    fullName: 'John Doe',
+    email: 'johndoe@mail.com'
   },
   hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    name: 'John Doe',
+    p: ['I develop 3D visuals, user', 'interfaces and web applications']
   },
   contact: {
-    p: "Get in touch",
-    h2: "Contact.",
+    p: 'Get in touch',
+    h2: 'Contact.',
     form: {
       name: {
-        span: "Your Name",
-        placeholder: "What's your name?",
+        span: 'Your Name',
+        placeholder: "What's your name?"
       },
-      email: { span: "Your Email", placeholder: "What's your email?" },
+      email: { span: 'Your Email', placeholder: "What's your email?" },
       message: {
-        span: "Your Message",
-        placeholder: "What do you want to say?",
-      },
-    },
+        span: 'Your Message',
+        placeholder: 'What do you want to say?'
+      }
+    }
   },
   sections: {
     about: {
-      p: "Introduction",
-      h2: "Overview.",
+      p: 'Introduction',
+      h2: 'Overview.',
       content: `I'm a skilled software developer with experience in TypeScript and
       JavaScript, and expertise in frameworks like React, Node.js, and
       Three.js. I'm a quick learner and collaborate closely with clients to
       create efficient, scalable, and user-friendly solutions that solve
-      real-world problems. Let's work together to bring your ideas to life!`,
+      real-world problems. Let's work together to bring your ideas to life!`
     },
     experience: {
-      p: "What I have done so far",
-      h2: "Work Experience.",
+      p: 'What I have done so far',
+      h2: 'Work Experience.'
     },
     feedbacks: {
-      p: "What others say",
-      h2: "Testimonials.",
+      p: 'What others say',
+      h2: 'Testimonials.'
     },
     works: {
-      p: "My work",
-      h2: "Projects.",
+      p: 'My work',
+      h2: 'Projects.',
       content: `Following projects showcases my skills and experience through
     real-world examples of my work. Each project is briefly described with
     links to code repositories and live demos in it. It reflects my
     ability to solve complex problems, work with different technologies,
-    and manage projects effectively.`,
-    },
-  },
-};
+    and manage projects effectively.`
+    }
+  }
+}
