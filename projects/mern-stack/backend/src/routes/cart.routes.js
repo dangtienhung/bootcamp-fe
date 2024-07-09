c3ecbe6e654f355ecf59c5e5d42ea02fc3878ba8
dangtienhung <danghung02042003@gmail.com>
2024-07-09 23:04:27 +0700

    create order

--- projects/mern-stack/backend/src/routes/cart.routes.js
+++ projects/mern-stack/backend/src/routes/cart.routes.js
@@ -6,11 +6,15 @@ import { wrapRequestHandler } from '../utils/handlers.util.js';
 
 const router = express.Router();
 
+// add to cart
 router.post(
   '/cart',
   wrapRequestHandler(verifyToken),
   wrapRequestHandler(addToCartMiddleware),
   wrapRequestHandler(cartController.addCart),
 );
+
+// get carts by userId
+router.get('/cart', wrapRequestHandler(verifyToken), wrapRequestHandler(cartController.getCartByUserId));
 
 export default router;
--- projects/mern-stack/backend/src/services/cart.service.js
+++ projects/mern-stack/backend/src/services/cart.service.js
@@ -2,8 +2,14 @@ import Cart from '../models/cart.model.js';
 
 export const cartService = {
   // get carts by userId
-  getCartsByUserId: async (userId) => {
-    return Cart.findOne({ userId });
+  getCartsByUserId: async (query, params) => {
+    if (params) {
+      return Cart.findOne({ userId: query.userId }).populate([
+        { path: 'userId', select: '_id email avatar fullname phone status' },
+        { path: 'carts.productId', select: '_id nameProduct price sale images is_deleted status' },
+      ]);
+    }
+    return Cart.findOne({ userId: query.userId });
   },
   // createCart
   createCart: async (userId, carts) => {
