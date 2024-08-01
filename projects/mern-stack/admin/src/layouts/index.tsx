0fc7a19e075831f67606c2aae42dbfa9d66f9949
dangtienhung <danghung02042003@gmail.com>
2024-08-01 23:05:26 +0700

    feat: crud useContext

--- projects/mern-stack/admin/src/layouts/index.tsx
+++ projects/mern-stack/admin/src/layouts/index.tsx
@@ -1,9 +1,18 @@
+import { CounterProvider } from '@/contexts/counter-context'
+import { ProductProvider } from '@/contexts/product-context'
+import { TProduct } from '@/types/product.type'
 import { Layout } from 'antd'
 import { Outlet } from 'react-router-dom'
 import Header from './components/header'
 import Sidebar from './components/sidebar'
 
 const RootLayout = () => {
+  const data: TProduct[] = [
+    { id: '1', name: 'Product 1', price: 100 },
+    { id: '2', name: 'Product 2', price: 200 },
+    { id: '3', name: 'Product 3', price: 300 }
+  ]
+
   return (
     <Layout className='!h-screen'>
       <Layout.Sider width='250px' className='!bg-white'>
@@ -14,7 +23,11 @@ const RootLayout = () => {
           <Header />
         </Layout.Header>
         <Layout.Content>
-          <Outlet />
+          <CounterProvider>
+            <ProductProvider data={data}>
+              <Outlet />
+            </ProductProvider>
+          </CounterProvider>
         </Layout.Content>
       </Layout>
     </Layout>
--- projects/mern-stack/admin/src/pages/products/index.tsx
+++ projects/mern-stack/admin/src/pages/products/index.tsx
@@ -1,5 +1,57 @@
+import { Button, Space, Table } from 'antd'
+
+import useCounter from '@/contexts/counter-context'
+import { useProduct } from '@/contexts/product-context'
+import { TProduct } from '@/types/product.type'
+import { DeleteOutlined } from '@ant-design/icons'
+
 const ProductPage = () => {
-  return <div>ProductPage</div>
+  const { products, handleDeleteProduct } = useProduct()
+  const { counter } = useCounter()
+
+  const columns = [
+    {
+      title: 'ID Sản phẩm',
+      dataIndex: 'id',
+      key: 'id'
+    },
+    {
+      title: 'Tên sản phẩm',
+      dataIndex: 'name',
+      key: 'name'
+    },
+    {
+      title: 'Giá sản phẩm',
+      dataIndex: 'price',
+      key: 'price'
+    },
+    {
+      title: false,
+      dataIndex: 'action',
+      key: 'action',
+      render: (_: undefined, product: TProduct & { key: string }) => {
+        return (
+          <Space>
+            <Button danger onClick={() => handleDeleteProduct(product.id)}>
+              <DeleteOutlined />
+            </Button>
+          </Space>
+        )
+      }
+    }
+  ]
+
+  const dataSource = products.map((product) => ({
+    ...product,
+    key: product.id
+  }))
+
+  return (
+    <div>
+      Counter: {counter}
+      <Table dataSource={dataSource} columns={columns} />
+    </div>
+  )
 }
 
 export default ProductPage
